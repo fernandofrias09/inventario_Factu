4a2c78cb2d821fe6ea260974a894c265aac78800
DESKTOP-6FRLIST\jose1516 <fernandofrias09@gmail.com>
2020-05-15 18:49:59 -0400

    Creacion de la clase conexion, solamente el metodo conectar

---
+++ Inventario_facturacion/Conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Inventario_facturacion
+{
+    class Conexion
+    {
+        public static SqlConnection Conectar()
+        {
+            SqlConnection cn = new SqlConnection("SERVER=FERNANDOFRIAS;DATABASE=Tienda_Globos;integrated security = true");
+            cn.Open();           
+            return cn;
+        }
+    }
+}
